c65ee198cc485c777f8e9f4df1c5bcc624dfa620
Samuel Xavier <sancrisxa@gmail.com>
2025-03-06 23:21:08 -0300

    first commit

---
+++ src/main/java/br/com/sancrisxa/api_consulta_creditos/ApiConsultaCreditosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.api_consulta_creditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaCreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaCreditosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sancrisxa/api_consulta_creditos/ApiConsultaCreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sancrisxa.api_consulta_creditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaCreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
